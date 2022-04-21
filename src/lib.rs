1b8911113935f52c4e4424e4f8658abb00de83c3
Riley Wilton <riley.j.wilton@gmail.com>
2022-04-21 14:30:30 -0400

    Basic serial support

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ extern crate core;
 extern crate vexv5rt;
 extern crate newlib_alloc;
 
+// Modules that we define
+mod internal; // Internal functions that the user should not use
+pub mod serial; // Actual serial implementation
---
+++ src/serial.rs
@@ -0,0 +1,77 @@
+
+
+
+
+use core::prelude::rust_2021::*;
+use alloc::vec::Vec;
+use acid_io::{Read, Write};
+
+/// Basic serial Read/Write implementation
+/// with buffering
+#[derive(Default)]
+pub struct Serial {
+    buffer: Vec<u8>,
+    write_buffer: Vec<u8>
+
+}
+
+impl Serial {
+    pub fn new() -> Serial {
+        Serial { buffer: Vec::new(), write_buffer: Vec::new() }
+    }
+}
+
+
+impl Read for Serial {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, acid_io::Error> {
+        // Read until we have enough bytes
+        while self.buffer.len() < buf.len() {
+
+            // Read in the data
+            let data = unsafe { vexv5rt::vexSerialReadChar(1) };
+
+            // If it is out of bounds exit the loop
+            if !(0..=0xff).contains(&data) {
+                break;
+            }
+
+            self.buffer.push(data as u8);
+        }
+        
+        // Figure how many bytes we have to copy over
+        let len = core::cmp::min(buf.len(), self.buffer.len());
+
+        // Drain len bytes from the buffer
+        let mut data: Vec<u8> = self.buffer.drain(0..len).collect();
+        
+        // Make sure the size is buf.len()
+        data.resize(buf.len(), 0u8);
+
+        // Copy into buf
+        buf.copy_from_slice(&data);
+
+        Ok(len)
+    }
+}
+
+
+impl Write for Serial {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, acid_io::Error> {
+
+        // Push the data to the buffer
+        self.write_buffer.extend_from_slice(buf);
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> Result<(), acid_io::Error> {
+
+        // Flush the entire buffer to serial
+        unsafe { crate::internal::send_serial_raw(self.buffer.clone()); }
+
+        // Clear the buffer
+        self.write_buffer.clear();
+
+        Ok(())
+    }
+}
