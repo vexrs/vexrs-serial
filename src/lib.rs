31370a6c6a1b5c7fce3ffed8275e85709cf431bc
Riley Wilton <riley.j.wilton@gmail.com>
2022-04-28 13:43:10 -0400

    Finished removing v5 support

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,5 @@
 extern crate alloc;
 extern crate core;
 
-// Modules that we define
-#[cfg(feature = "v5")]
-pub mod internal; // Internal functions that the user should not use
-#[cfg(feature = "v5")]
-pub mod serial; // Actual serial implementation
 
 pub mod protocol; // Contains the basic protocol implementation
